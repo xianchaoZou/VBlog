ff41300968a72d22fbd631fd175bba2c81ee8a5b
xianchaoZou <65705368+xianchaoZou@users.noreply.github.com>
2020-10-18 20:30:28 +0800

    Update BlogserverApplication.java
    测试

--- blogserver/src/main/java/org/sang/BlogserverApplication.java
+++ blogserver/src/main/java/org/sang/BlogserverApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling//开启定时任务支持
+@EnableScheduling//开启定时任务支持1
 public class BlogserverApplication {
 
     public static void main(String[] args) {
